8742ac5170e6db857dfd1752741d3c1631d5a218
Rahul kumar <rahulparida933@gamil.com>
2020-08-09 16:21:37 +0530

    colors added

--- app/src/main/java/com/sih/todolist_room/MainActivity.kt
+++ app/src/main/java/com/sih/todolist_room/MainActivity.kt
@@ -1,14 +1,30 @@
 package com.sih.todolist_room
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolBar)
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
 
-
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.history -> {
+                startActivity(Intent(this,HistoryActivity::class.java))
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
